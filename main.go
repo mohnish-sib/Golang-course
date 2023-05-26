f5bf486109d2157d0a080a71bc9844266d2c3722
mohnish-sib <mohnish.lokhande@sendinblue.com>
2023-05-26 09:16:36 +0530

    age

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -12,13 +13,38 @@ var reader *bufio.Reader
 func main()  {
 
 	reader =bufio.NewReader(os.Stdin)
-	fmt.Println("Hello")
+
+	userName := readSting("Hello")
+	age:=readInt("Your age?")
+
+	fmt.Println("Your name is",userName, "and your age is", age)
+	
+}
+
+func readSting(s string) string  {
+	fmt.Println(s)
 	fmt.Print("-> ")
 
 	userInput, _ := reader.ReadString('\n')
 	userInput =strings.Replace(userInput,"\r\n","",-1)
 	userInput =strings.Replace(userInput,"\n","",-1)
 
-	fmt.Println("Your name is",userInput)
-	
+	return userInput
+}
+
+func readInt(s string) int  {
+	fmt.Println(s)
+	fmt.Print("-> ")
+
+	userInput, _ := reader.ReadString('\n')
+	userInput =strings.Replace(userInput,"\r\n","",-1)
+	userInput =strings.Replace(userInput,"\n","",-1)
+
+	num, err:=strconv.Atoi(userInput)// conert string into number
+
+	if err != nil{
+		fmt.Println("Please enter a whole number")
+	}
+
+	return num
 }
